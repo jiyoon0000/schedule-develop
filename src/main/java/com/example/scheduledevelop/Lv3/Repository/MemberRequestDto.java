c49a387aa170944e9feafb8a84f473490cf28bfd
jiyoon <rlawldbs9128@gmail.com>
2024-11-15 01:37:19 +0900

    feat(MemberRequestDto) : MemberRequestDto 생성, 회원가입 시 유저한테 요청할 dto

---
+++ src/main/java/com/example/scheduledevelop/Lv3/Repository/MemberRequestDto.java
@@ -0,0 +1,18 @@
+package com.example.scheduledevelop.Lv3.Repository;
+
+import lombok.Getter;
+
+@Getter
+
+//회원가입 시 필요한 유저의 정보들 전달받기 위한 dto
+public class MemberRequestDto {
+    private String username;
+    private String email;
+    private String password;
+
+    public MemberRequestDto(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}

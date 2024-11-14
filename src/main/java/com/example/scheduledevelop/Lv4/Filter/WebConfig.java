fc1a773a9475129ad62c354a7a05e2342b4b6876
jiyoon <rlawldbs9128@gmail.com>
2024-11-15 03:04:31 +0900

    feat(WebConfig) : WebConfig 생성해서 URL 패턴 설정, 필터 우선순위, 모든 URL 패턴에 필터 적용

---
+++ src/main/java/com/example/scheduledevelop/Lv4/Filter/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.scheduledevelop.Lv4.Filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean<LoginFilter> loginFilter(){
+        FilterRegistrationBean<LoginFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}

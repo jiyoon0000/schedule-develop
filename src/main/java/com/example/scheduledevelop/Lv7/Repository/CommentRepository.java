40f90f53074e0c03ae55449cd4721aba56fd7475
jiyoon <rlawldbs9128@gmail.com>
2024-11-15 12:31:08 +0900

    docs : CommentRepository 주석 추가

--- src/main/java/com/example/scheduledevelop/Lv7/Repository/CommentRepository.java
+++ src/main/java/com/example/scheduledevelop/Lv7/Repository/CommentRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+//댓글 데이터베이스에 접근할 수 있는 저장소
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    //특정 일정에 관련된 댓글 목록을 조회
     List<Comment> findByScheduleId(Long scheduleId);
 }

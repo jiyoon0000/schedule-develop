8ad12c6c74a217c1a2a6b7a14e4496c99caa7472
jiyoon <rlawldbs9128@gmail.com>
2024-11-15 12:26:12 +0900

    feat(Comment) : 댓글 Entity 생성

---
+++ src/main/java/com/example/scheduledevelop/Lv7/Entity/Comment.java
@@ -0,0 +1,60 @@
+package com.example.scheduledevelop.Lv7.Entity;
+
+import com.example.scheduledevelop.Lv3.Entity.Member;
+import com.example.scheduledevelop.Lv3.Entity.Schedule;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor //기본생성자 생성
+@Entity
+@Table(name = "comments")
+@EntityListeners(AuditingEntityListener.class)
+public class Comment {
+
+    @Id
+    //기본 키 자동 생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //내용은 필수 입력ㄱ값
+    @Column(nullable = false)
+    private String content;
+
+    //댓글 작성 시 자동으로 생성일 추가
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    //댓글 작성 시 자동으로 수정일 변경
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+    //댓글-유저 다:1 관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Member user;
+
+    //댓글-일정 다:1 관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id", nullable = false)
+    private Schedule schedule;
+
+    //생성자 : 초기화
+    public Comment(String content, Member user, Schedule schedule) {
+        this.content = content;
+        this.user = user;
+        this.schedule = schedule;
+    }
+
+    //댓글내용 업데이트
+    public void updateContent(String content){
+        this.content = content;
+    }
+}

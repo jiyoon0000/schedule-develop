1523c3364d46a980e219fbaebb0464e95e6ddd1e
jiyoon <rlawldbs9128@gmail.com>
2024-11-15 11:53:56 +0900

    refactor(ScheduleRequsetDto) : 예외처리

--- src/main/java/com/example/scheduledevelop/Lv5/dto/ScheduleRequestDto.java
+++ src/main/java/com/example/scheduledevelop/Lv5/dto/ScheduleRequestDto.java
@@ -1,12 +1,17 @@
 package com.example.scheduledevelop.Lv5.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class ScheduleRequestDto {
 
+    @NotBlank(message = "제목은 필수 입력값입니다.")
+    @Size(max = 10, message = "제목은 10글자 이내로 입력해야합니다.")
     private String title;
 
+    @NotBlank(message = "내용은 필수 입력값입니다.")
     private String contents;
 
     public ScheduleRequestDto(String title, String contents) {
